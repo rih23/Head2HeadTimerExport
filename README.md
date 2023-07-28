89b016d7178c73f40e4ecc668ab6df829d0bc4dd
rih23 <rihardsblekus2002@gmail.com>
2023-07-29 01:13:12 +0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Head2HeadTimerExport
-A Celeste mod that exports the race results from a Head2Head race
---
+++ TimerExportModuleSession.cs
@@ -0,0 +1,5 @@
+namespace Celeste.Mod.TimerExport {
+    public class TimerExportModuleSession : EverestModuleSession {
+
+    }
+}
